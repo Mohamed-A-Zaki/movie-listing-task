74e85f44340e450c27038352233ff48794ceb45c
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-12-01 02:26:37 +0200

    add comments

--- atoms/movies-atom.ts
+++ atoms/movies-atom.ts
@@ -5,21 +5,47 @@ export const moviesAtom = atom<MoviesAtom, MovieAtomActions>({
   key: "movies-atom",
 
   default: {
+    /***
+     * movies
+     */
     movies: [],
+
+    /***
+     * isLoading
+     */
     isLoading: true,
+
+    /***
+     * error
+     */
     error: "",
   },
 
   actions: {
+    /***
+     * start loading action
+     */
     startLoading() {
       moviesAtom.change("isLoading", true);
     },
+
+    /***
+     * stop loading action
+     */
     stopLoading() {
       moviesAtom.change("isLoading", false);
     },
+
+    /***
+     * set error action
+     */
     setError(error: string) {
       moviesAtom.change("error", error);
     },
+
+    /***
+     * set movies action
+     */
     setMovies(movies: Movie[]) {
       moviesAtom.change("movies", movies);
     },
